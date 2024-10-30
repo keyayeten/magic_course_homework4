e7b47cd0db346ee281bb4034b1d9fd63b4cf174f
Zhukovton <zhukovton@gmail.com>
2024-10-30 16:36:29 +0300

    Добавил домашку 4

--- task1/task1.py
+++ task1/task1.py
@@ -1,9 +1,22 @@
+import pathlib
+
 # Представьте, что вы решили создать личную цифровую
 # библиотеку. Напишите программу,
 # которая создает директорию library,
 # а внутри — папки для жанров: fiction, non-fiction,
 # history, fantasy. Это будет ваш цифровой архив книг.
 
+folders = ["fiction", "non-fiction", "history", "fantasy"]
+dir = "library"
 
-def create_library():
-    pass
+
+def create_library(dir, folders):
+    path = pathlib.Path(dir)
+    path.mkdir(exist_ok=True)
+
+    for i in folders:
+        folders_path = path / i
+        folders_path.mkdir(exist_ok=True)
+
+
+create_library(dir, folders)
--- task4/task4.py
+++ task4/task4.py
@@ -3,11 +3,27 @@
 # которая проверяет, существует ли этот файл,
 # чтобы убедиться, что ваша музыкальная коллекция
 # не потеряна.
+import os
+
 
 def is_file_exist(filename: str):
-    pass
+    files_list = []
+    for adress, dirs, files in os.walk("C:\\Users\\a.zhukov\\Desktop\\magic\\magic_course_homework4"):
+        for file in files:
+            if filename == file:
+                files_list.append(file)
+                break
+    if filename in files_list:
+        return print(f"{filename} найден")
+    else:
+        return print(f"{filename} не найден")
+
+
+
+
 
 
 if __name__ == "__main__":
     filename = "favorite_playlist.txt"
-    print(filename, is_file_exist(filename))
+    # print(filename, is_file_exist(filename))
+    is_file_exist(filename)
