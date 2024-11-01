7aa54541d2e23dcb83ea13af6719fbc27989daae
qapmaria <mdgrishunina@gmail.com>
2024-11-01 13:51:02 +0300

    task3-4

--- task3/task3.py
+++ task3/task3.py
@@ -4,9 +4,16 @@
 # Это полезно для анализа книг или длинных текстов.
 
 
-def find_most_frequent(filename):
-    pass
+from collections import Counter
 
+def find_most_frequent(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        content = file.read().lower()
+    words = content.split()
+    frequent_words = Counter(words)
+    most_common_words = frequent_words.most_common(10)
+    return most_common_words
 
 if __name__ == "__main__":
     print(find_most_frequent("text.txt"))
+    
