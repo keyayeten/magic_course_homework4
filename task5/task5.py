49699aff6ffaa31916da7676e3dc13c2ed679bdd
qapmaria <mdgrishunina@gmail.com>
2024-11-01 13:52:06 +0300

    task5-4

--- task5/task5.py
+++ task5/task5.py
@@ -4,11 +4,19 @@
 # recipes_backup.txt.
 
 
+import pathlib
+import shutil
 def copy_recipes(filename, new_filename):
-    pass
+    path = pathlib.Path(filename)
+    if not path.exists():
+        print(f"Файл {filename} не найден")
+        return
+    else:
+        return shutil.copy(filename, new_filename)
 
 
 if __name__ == "__main__":
     filename = "recipes.txt"
-    backup_filename = "recipes_backup.txt"
-    copy_recipes(filename, backup_filename)
+    new_filename = "recipes_backup.txt"
+    copy_recipes(filename, new_filename)
+    
